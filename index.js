31894965d805184d6d449db676fe07c70853f545
Mike Wernicke <mike.wernicke@gmail.com>
2017-09-29 23:47:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,19 +10,52 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var itemPrice = Math.floor(Math.random() * 100 + 1);
+  console.log(item + " has been added to your cart.");
+  cart.push( { [item] : itemPrice } )
+  return cart
 }
 
 function viewCart() {
-  // write your code here
+  var cartListing = [];
+if (cart.length === 0) {
+  return console.log("Your shopping cart is empty.");
+}
+else {
+  for(var i = 0; i < cart.length; i++) {
+    var itemName = object.keys(cart[i]);
+    var itemPrice = cart[i];
+    //if(i < cart.length - 1) {
+    //  var itemPhrase = itemName + " at $" + itemPrice;
+    //}
+    //else if(i = cart.length - 1) {
+    //  itemPhrase = " and " + itemName + " at $" + itemPrice;
+    //}
+    //cartListing.push(itemPhrase);
+    }
+  }
+  return console.log("In your cart, you have " + cartListing + ".");
 }
 
 function total() {
-  // write your code here
+  var totalCost = 0;
+  for (var i = 0; i < cart.length; i++) {
+    totalCost =+ Number.parseInt(cart[i], [10] );
+  }
+  return totalCost;
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for(var i = 0; i < cart.length; i++) {
+    if (cart[i].hasOwnProperty(item)) {
+      cart.splice(i,1);
+      return cart;
+    }
+    else {
+      console.log("That item is not in your cart.");
+      return cart;
+    }
+  }
 }
 
 function placeOrder(cardNumber) {
